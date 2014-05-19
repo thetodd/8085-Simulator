5328edcfabde1ae9d8a86321863940dff29b9979
thetodd <florian.schleich@gmail.com>
2014-05-19 19:53:05 +0200

    support for hard coded addresses

--- src/de/thetodd/simulator8085/api/mnemonics/JMPMnemonic.java
+++ src/de/thetodd/simulator8085/api/mnemonics/JMPMnemonic.java
@@ -8,12 +8,16 @@ import de.thetodd.simulator8085.api.Simulator;
 
 public class JMPMnemonic implements Mnemonic {
 
-	//TODO: Unterstützung von hardcoded Adressen
 	@Override
 	public byte[] getOpcode(String[] arguments) throws IllegalArgumentException {
 		byte[] opcode = new byte[3];
 		opcode[0] = (byte) 0xC3;
-		short adr = Simulator.getInstance().getLabelAdress(arguments[0]);
+		short adr = 0;
+		try {
+			adr = Integer.decode(arguments[0]).shortValue(); //support for hard coded addresses
+		} catch (Exception ex) {
+			adr = Simulator.getInstance().getLabelAdress(arguments[0]);
+		}
 		opcode[2] = (byte) ((adr & 0xFF00) >> 8);
 		opcode[1] = (byte) ((adr & 0x00FF));
 		return opcode;
