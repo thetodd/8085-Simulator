46aab8617d04709bec0005fc7c9f4dc72a43ec6f
thetodd <florian.schleich@gmail.com>
2014-05-29 22:33:50 +0200

    subtraction mnemonics support

---
+++ src/de/thetodd/simulator8085/api/mnemonics/SBBMnemonic.java
@@ -0,0 +1,99 @@
+package de.thetodd.simulator8085.api.mnemonics;
+
+import de.thetodd.simulator8085.api.Memory;
+import de.thetodd.simulator8085.api.Mnemonic;
+import de.thetodd.simulator8085.api.Processor;
+
+public class SBBMnemonic implements Mnemonic {
+
+	public byte[] getOpcode(String[] arguments) {
+		byte[] opcode = new byte[1];
+		char arg = arguments[0].toLowerCase().charAt(0);
+		switch (arg) {
+		case 'a':
+			opcode[0] = (byte) 0x9F;
+			break;
+		case 'm':
+			opcode[0] = (byte) 0x9E;
+			break;
+		case 'b':
+			opcode[0] = (byte) 0x98;
+			break;
+		case 'c':
+			opcode[0] = (byte) 0x99;
+			break;
+		case 'd':
+			opcode[0] = (byte) 0x9A;
+			break;
+		case 'e':
+			opcode[0] = (byte) 0x9B;
+			break;
+		case 'h':
+			opcode[0] = (byte) 0x9C;
+			break;
+		case 'l':
+			opcode[0] = (byte) 0x9D;
+			break;
+		default:
+			throw new IllegalArgumentException("Argument " + arguments[0]
+					+ " wird nicht unterstuetzt!");
+		}
+
+		return opcode;
+	}
+
+	@Override
+	public void execute() {
+		byte opcode = Memory.getInstance().get(
+				Processor.getInstance().getProgramcounter());
+		Processor.getInstance().incProgramcounter();
+		byte b = 0;
+		switch (opcode) {
+		case (byte) 0x9F:
+			b = Processor.getInstance().getRegisterA();
+			break;
+		case (byte) 0x98:
+			b = Processor.getInstance().getRegisterB();
+			break;
+		case (byte) 0x99:
+			b = Processor.getInstance().getRegisterC();
+			break;
+		case (byte) 0x9A:
+			b = (Processor.getInstance().getRegisterD());
+			break;
+		case (byte) 0x9B:
+			b = (Processor.getInstance().getRegisterE());
+			break;
+		case (byte) 0x9C:
+			b = (Processor.getInstance().getRegisterH());
+			break;
+		case (byte) 0x9D:
+			b = (Processor.getInstance().getRegisterL());
+			break;
+		case (byte) 0x9E:
+			b = Memory.getInstance().get(Processor.getInstance().getRegisterHL());
+			break;
+		default:
+			break;
+		}
+
+		short a2 = (short) (Processor.getInstance().getRegisterA() & 0xFF);
+		short b2 = (short) (b & 0xFF);
+		int c = a2 - b2;
+		if(Processor.getInstance().isCarryFlag()) {
+			c -= 1;
+		}
+		Processor.getInstance().setRegisterA((byte) c);
+		Processor.getInstance().setFlags((short) c);
+	}
+
+	@Override
+	public boolean hasOpcode(byte opcode) {
+		return (opcode >= (byte) 0x98) && (opcode <= (byte) 0x9F);
+	}
+
+	@Override
+	public byte size() {
+		return 1;
+	}
+}
---
+++ src/de/thetodd/simulator8085/api/mnemonics/SUBMnemonic.java
@@ -0,0 +1,96 @@
+package de.thetodd.simulator8085.api.mnemonics;
+
+import de.thetodd.simulator8085.api.Memory;
+import de.thetodd.simulator8085.api.Mnemonic;
+import de.thetodd.simulator8085.api.Processor;
+
+public class SUBMnemonic implements Mnemonic {
+
+	public byte[] getOpcode(String[] arguments) {
+		byte[] opcode = new byte[1];
+		char arg = arguments[0].toLowerCase().charAt(0);
+		switch (arg) {
+		case 'a':
+			opcode[0] = (byte) 0x97;
+			break;
+		case 'm':
+			opcode[0] = (byte) 0x96;
+			break;
+		case 'b':
+			opcode[0] = (byte) 0x90;
+			break;
+		case 'c':
+			opcode[0] = (byte) 0x91;
+			break;
+		case 'd':
+			opcode[0] = (byte) 0x92;
+			break;
+		case 'e':
+			opcode[0] = (byte) 0x93;
+			break;
+		case 'h':
+			opcode[0] = (byte) 0x94;
+			break;
+		case 'l':
+			opcode[0] = (byte) 0x95;
+			break;
+		default:
+			throw new IllegalArgumentException("Argument " + arguments[0]
+					+ " wird nicht unterstuetzt!");
+		}
+
+		return opcode;
+	}
+
+	@Override
+	public void execute() {
+		byte opcode = Memory.getInstance().get(
+				Processor.getInstance().getProgramcounter());
+		Processor.getInstance().incProgramcounter();
+		byte b = 0;
+		switch (opcode) {
+		case (byte) 0x97:
+			b = Processor.getInstance().getRegisterA();
+			break;
+		case (byte) 0x90:
+			b = Processor.getInstance().getRegisterB();
+			break;
+		case (byte) 0x91:
+			b = Processor.getInstance().getRegisterC();
+			break;
+		case (byte) 0x92:
+			b = (Processor.getInstance().getRegisterD());
+			break;
+		case (byte) 0x93:
+			b = (Processor.getInstance().getRegisterE());
+			break;
+		case (byte) 0x94:
+			b = (Processor.getInstance().getRegisterH());
+			break;
+		case (byte) 0x95:
+			b = (Processor.getInstance().getRegisterL());
+			break;
+		case (byte) 0x96:
+			b = Memory.getInstance().get(Processor.getInstance().getRegisterHL());
+			break;
+		default:
+			break;
+		}
+
+		short a2 = (short) (Processor.getInstance().getRegisterA() & 0xFF);
+		short b2 = (short) (b & 0xFF);
+		int c = a2 - b2;
+		Processor.getInstance().setRegisterA((byte) c);
+		Processor.getInstance().setFlags((short) c);
+	}
+
+	@Override
+	public boolean hasOpcode(byte opcode) {
+		return (opcode >= (byte) 0x90) && (opcode <= (byte) 0x97);
+	}
+
+	@Override
+	public byte size() {
+		return 1;
+	}
+}
